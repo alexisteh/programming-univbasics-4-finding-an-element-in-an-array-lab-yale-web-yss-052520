007ceeeac44270cf2ef3b31d9b88cc27e30cfd95
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-10 03:13:10 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,12 +1,11 @@
 
 def find_element_index(array, value_to_find)
   counter = 0 
-  while counter < array.size do 
+  while counter < array.length do 
     if array[counter] == value_to_find 
       return counter
     else 
       counter += 1 
     end 
-    return nil 
   end 
 end
